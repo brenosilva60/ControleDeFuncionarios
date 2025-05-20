fa43bfa71d8d75ddcc780b23a871ac5f2a941192
Henrique Onorato <onoratok2@gmail.com>
2025-05-20 20:09:29 -0300

    wip

--- obj/Debug/net8.0/ControleDeFuncionarios.AssemblyInfo.cs
+++ obj/Debug/net8.0/ControleDeFuncionarios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControleDeFuncionarios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62fed41919f096a1f280565c5d40148f83469658")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControleDeFuncionarios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControleDeFuncionarios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
